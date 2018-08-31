ba3fa618d29097ea36c1ee806f3076a181d53bc3
MarenSte <marensteinkamp@gmx.net>
2018-08-31 22:00:44 +0200

    description deleted

--- neopixel.cpp
+++ neopixel.cpp
@@ -1,16 +1,5 @@
 #include "pxt.h"
 
-    /**
-     * Draws an image on the LED screen.
-     * @param leds the pattern of LED to turn on/off
-     * @param interval time in milliseconds to pause after drawing
-     */
-    //% help=basic/show-leds
-    //% weight=95 blockGap=8
-    //% imageLiteral=1 async
-    //% blockId=device_show_leds
-    //% block="show leds" icon="\uf00a"
-    //% parts="ledmatrix"
 
 namespace neopixel {
 
