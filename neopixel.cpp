f0b4b04e6f0593f2182424c1ec164cf5017ad7d1
MarenSte <marensteinkamp@gmx.net>
2018-08-31 21:49:10 +0200

    änderung showleds

--- neopixel.cpp
+++ neopixel.cpp
@@ -21,10 +21,8 @@ namespace neopixel {
      * @param leds the pattern of LED to turn on/off
      * @param interval time in milliseconds to pause after drawing
      */
-    //% help=basic/show-leds
     //% weight=95 blockGap=8
     //% imageLiteral=1 async
-    //% blockId=device_show_leds
     //% block="show leds" icon="\uf00a"
     //% parts="ledmatrix"
   void showLeds(ImageLiteral leds, int interval = 400) {
