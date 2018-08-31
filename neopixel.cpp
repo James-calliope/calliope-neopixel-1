f4e03099d2d68e35dea57655f1436938fc07dfd7
MarenSte <marensteinkamp@gmx.net>
2018-08-31 21:36:10 +0200

    ImageLiteral Test

---
+++ neopixel.cpp
@@ -0,0 +1,34 @@
+#include "pxt.h"
+
+    /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help=basic/show-leds
+    //% weight=95 blockGap=8
+    //% imageLiteral=1 async
+    //% blockId=device_show_leds
+    //% block="show leds" icon="\uf00a"
+    //% parts="ledmatrix"
+
+namespace neopixel {
+
+
+
+   /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help=basic/show-leds
+    //% weight=95 blockGap=8
+    //% imageLiteral=1 async
+    //% blockId=device_show_leds
+    //% block="show leds" icon="\uf00a"
+    //% parts="ledmatrix"
+  void showLeds(ImageLiteral leds, int interval = 400) {
+      uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, interval);
+    }
+
+}
