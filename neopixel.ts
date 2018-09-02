710ae16cfa96a09ed1c316f5879e5792d1c9d6ee
MarenSte <marensteinkamp@gmx.net>
2018-09-02 20:39:45 +0200

    Update neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -71,7 +71,7 @@ namespace neopixel {
         * @param value Beschreibe den Wert hier, eg: 5
         */    
         //% block
-        fib(value: number): number {
+        export function fib(value: number): number {
             return value <= 1 ? value : fib(value -1) + fib(value - 2);
         }
         
